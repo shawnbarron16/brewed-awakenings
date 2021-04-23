4f3f234d92aa067487e96cd4cbd4d030203ae238
shawnbarron16 <shawnbarron16@gmail.com>
2021-04-23 10:25:47 -0500

    First part of click listener of employees

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,12 +1,28 @@
 import { getEmployees } from "./database.js"
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const employeeClicked = clickEvent.target
+        if(employeeClicked.id.startsWith("employee")) {
+            const [,employeeId] = employeeClicked.id.split("--")
+
+            for(const employee of employees) {
+                if(employee.id === parseInt(employeeId)) {
+                    window.alert(`${employee.name} has sold x number of products`)
+                }
+            }
+        }
+    }
+)
+
 const employees = getEmployees()
 
 export const Employees = () => {
     let html = "<ul>"
 
     for (const employee of employees) {
-        html += `<li id="employee--${employee.Id}">${employee.name}</li>`
+        html += `<li id="employee--${employee.id}">${employee.name}</li>`
     }
 
     html += "</ul>"
